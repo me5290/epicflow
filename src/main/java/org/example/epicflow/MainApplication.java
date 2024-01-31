320d7bbb6c0c9ef0844e441fa86085d20dc6b094
KDH <lococo0425@naver.com>
2024-01-31 16:53:48 +0900

    변경

--- src/main/java/org/example/epicflow/MainApplication.java
+++ src/main/java/org/example/epicflow/MainApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("test.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("battle.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("EpicFlow");
         stage.setScene(scene);
