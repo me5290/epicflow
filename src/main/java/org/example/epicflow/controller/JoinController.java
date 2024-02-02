73238cecb8e277b038a250c5fdca7da604603c52
sere4580@naver.com <sere4580@naver.com>
2024-02-02 17:02:09 +0900

    hyo_branch_village_test,JOIN

--- src/main/java/org/example/epicflow/controller/JoinController.java
+++ src/main/java/org/example/epicflow/controller/JoinController.java
@@ -1,9 +1,14 @@
 package org.example.epicflow.controller;    // 패키지명
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.example.epicflow.model.dao.MemberDao;
 import org.example.epicflow.model.dto.MemberDto;
 
@@ -11,12 +16,16 @@ public class JoinController {   // Class Start
 
 // ================================ 회원가입 s ================================ //
     @FXML
-    public TextArea mid;
+    public TextField mid;
     @FXML
-    public TextArea mpw;
+    public TextField mpw;
+    @FXML
+    public Button loginScreen;
+
 
     // ===== 회원가입 메소드
     public void signUp(){
+
         String mid = this.mid.getText();
         String mpw = this.mpw.getText();
 
@@ -26,13 +35,13 @@ public class JoinController {   // Class Start
         // ===== 객체 넘기기
         MemberDao.getInstance().signUp(playerDto);
 
-//        System.out.println(playerDto.toString());
-//        System.out.println(this.mid);
-//        System.out.println(this.mpw);
-//        System.out.println(this.mid.getText());
-//        System.out.println(this.mpw.getText());
+        System.out.println(playerDto.toString());
+        System.out.println(this.mid.getText());
+        System.out.println(this.mpw.getText());
 
         Msg("회원가입에 성공하였습니다.");
+        loginScreen();
+
     }
 
     // ===== 회원가입 알림창
@@ -43,4 +52,23 @@ public class JoinController {   // Class Start
         alert.show();
     }
 // ================================ 회원가입 e ================================ //
+// ================================ 화면전환 s ================================ //
+    // ===== 회원가입 화면 -> 로그인 화면
+    public void loginScreen(){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root);
+            Stage primaryStage = (Stage)loginScreen.getScene().getWindow();
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+
+
+
+
 } //  Class End
