e198bdf53fc5d13ba2a06dddd30464a5b0a9087c
KDH <lococo0425@naver.com>
2024-02-05 16:04:28 +0900

    2024-02-05 오후 04:03 취합

--- src/main/java/org/example/epicflow/MainApplication.java
+++ src/main/java/org/example/epicflow/MainApplication.java
@@ -19,6 +19,6 @@ public class MainApplication extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        // 2024-02-05 오후 04:03 취합
+        // 2024-02-05 오후 04:03 취합 asd
     }
 }
