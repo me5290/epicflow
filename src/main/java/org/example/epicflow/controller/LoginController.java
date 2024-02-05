3b4dc23bfe6b1e7979d87431b07b15cf78ca7c0e
gusdl1992 <tlgusdl92@gmail.com>
2024-02-05 18:13:00 +0900

    20240205 18:12 박시현

--- src/main/java/org/example/epicflow/controller/LoginController.java
+++ src/main/java/org/example/epicflow/controller/LoginController.java
@@ -14,8 +14,9 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.example.epicflow.MainApplication;
 import org.example.epicflow.model.dao.MemberDao;
+import org.example.epicflow.model.dao.PlayerDao;
 import org.example.epicflow.model.dto.MemberDto;
-
+import org.example.epicflow.model.dto.PlayerDto;
 import java.io.IOException;
 
 
@@ -35,7 +36,6 @@ public class LoginController extends MainApplication {
     public TextField cName;             // 로그인 성공시 캐릭터 이름 받기
     public Button generation;           // 캐릭터 생성 버튼
     public Button cancel;               // 캐릭터 생성 뷰 에서 취소버튼
-    public boolean bolean = false;      // 불리언 선언 조건부
 
     // 회원번호 get
     public static int getMemberNum() {
@@ -72,6 +72,7 @@ public class LoginController extends MainApplication {
             // 3. Dao 객체 넘기기
             memberNum = MemberDao.getInstance().Onlogin(memberDto);
             this.memberNum = memberNum;
+            System.out.println("memberNum = " + memberNum);
             // 4. 처리
             if (getMemberNum() == 0 ){
                 // Pane 뛰워서 존재하지않는 회원 표시
@@ -81,17 +82,33 @@ public class LoginController extends MainApplication {
                 inputPw.setText("");
                 break;
             }else {
-                // Pane 뛰워서 현재 로그인한 회원 정보 표시
-                memberView.setVisible(true);
-                loginLabel.setText(memberDto.getMid() + "회원님 환영 합니다.");
-                System.out.println(memberNum);
-                bolean = true;
-                System.out.println("OK버튼" + ok);
-                break;
-            }
+                // 로그인 성공 시 DB에 캐릭터 있는지 비교
+//                memberView.setVisible(true);
+//                loginLabel.setText(memberDto.getMid() + "회원님 환영 합니다.");
+//                System.out.println(memberNum);
+//                bolean = true;
+//                System.out.println("OK버튼" + ok);
 
+                // 캐릭터 존재 여부 확인 메소드
+                if (characterFind()){
+                    // 캐릭터 있으면 마을 씬 이동
+                    villageScene();
+                }else {
+                    // 캐릭터 없으면 캐릭터 생성 뷰 생성
+                    inputId.setText("");
+                    inputPw.setText("");
+                    System.out.println("캐릭터생성 뷰 보이기");
+                    System.out.println(cNamePane);
+                    cNamePane.setVisible(true);
+                } // 2 if else
+                break;
+            }// 1  if else 끝
         } // w e
-        // 로그인 성공! 배틀신으로 이동
+        
+
+    }
+    // 로그인 성공! 및 캐릭터 생성 완료 시 마을씬 이동
+    public void villageScene(){
         try {
             Parent battle = FXMLLoader.load(getClass().getResource("village_test.fxml"));
             Scene scene = new Scene(battle, 800, 600);
@@ -101,11 +118,7 @@ public class LoginController extends MainApplication {
             System.out.println("primaryStage = " + primaryStage);
             System.out.println("scene = " + scene);
         }catch (Exception e){
-            System.out.println(e);
-        }
-        // 배틀 씬 이동
-        if (bolean){
-            //battleScen();
+            System.out.println("villageScene : " + e);
         }
 
     }
@@ -143,17 +156,44 @@ public class LoginController extends MainApplication {
 
     }
 
-    // 캐릭터 생성 버튼 클릭 시
-    public void characterGeneration(){
 
+    // 캐릭터 유무 확인 메서드
+    public boolean characterFind(){
+        boolean ch = PlayerDao.getInstance().characterFind(memberNum);
+        System.out.println("캐릭터 찾기 ch = " + ch);
 
-
+        // 캐릭터 있으면 트루
+        if(ch){
+            return true;
+        }
+        // 캐릭터 없으면 false
+        return false;
     }
 
+
+    // 캐릭터 생성 버튼 클릭 시
+    public void characterGeneration(){
+        System.out.println("cName = " + cName.getText());
+        PlayerDto playerDto = new PlayerDto();
+        playerDto.setPname(cName.getText());
+        playerDto.setMno(memberNum);
+        System.out.println(playerDto.toString());
+
+        // Dao 에 객체 넘기기
+        boolean result = PlayerDao.getInstance().characterGeneration(playerDto);
+
+        // 결과 처리
+        if (result){
+            // 캐릭터 생성 성공!! 마을로 이동
+            villageScene();
+        }else {
+            System.out.println("캐릭터 생성 실패!");
+        }
+    }
     // 캐릭터 생성 뷰 에서 취소 클릭 시
     public void characterCancel(){
-//        System.out.println("캐릭터생성 뷰 안보이기");
-//        cNamePane.setVisible(false);
+        System.out.println("캐릭터생성 뷰 안보이기");
+        cNamePane.setVisible(false);
 
     }
 
--- src/main/java/org/example/epicflow/model/dao/PlayerDao.java
+++ src/main/java/org/example/epicflow/model/dao/PlayerDao.java
@@ -49,5 +49,53 @@ public class PlayerDao extends Dao{
         }
         return playerDtos;
     }
+
+    // 캐릭터 유무 확인 메서드
+    public boolean characterFind(int memberNum){
+        try {
+            // 1. sql 작성
+            String sql = "select * from player where mno = ?";
+            // 2. sql 기재
+            ps = con.prepareStatement(sql);
+            // ? 매개변수 대입
+            ps.setInt(1,memberNum);
+            // sql 실행
+            rs = ps.executeQuery();
+            // System.out.println("rs = " + rs);
+
+            // 캐릭터를 찾으면 true 반환
+            if(rs.next()){
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println("characterFind : " + e);
+        }
+        // Player DB 에서 회원 번호 없으면 (캐릭터 없는것) false 반환
+        return false;
+
+    }
+
+    //회원 가입 후 캐릭터 생성 버튼 클릭시 메서드
+    public boolean characterGeneration(PlayerDto playerDto){
+        try {
+            // sql 작성
+            String sql = "insert into player(pname,mno)values( ?, ? )";
+            // sql 기재
+            ps = con.prepareStatement(sql);
+            // ? 매개변수 대입
+            ps.setString(1,playerDto.getPname());
+            ps.setInt(2,playerDto.getMno());
+            // sql 실행
+            int count = ps.executeUpdate();
+            System.out.println("캐릭터 생성 count = " + count);
+
+            // DB에 INSERT 처리된 레코드가 1개이면 성공 0 이면 실패
+            if (count == 1){ return true; }
+        }catch (Exception e){
+            System.out.println("characterGeneration : " + e );
+        }
+        // 0 이면 실패
+        return false;
+    }
 }
 
